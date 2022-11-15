803a17e81488386037428d92b030b55cb1b4438a
Jesper Lindström <jesperlindstrom10@gmail.com>
2022-11-15 09:54:49 +0100

    added stop btn to cancel background change

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 window.addEventListener("DOMContentLoaded" , main);
 
+let timeoutRef;
+
 function main() {
     // start på programmet
 
-    setTimeout(changeBackgroundColor, 4000);
+   timeoutRef = setTimeout(changeBackgroundColor, 2000);
+   addEventListeners();
 }
 
 
 function changeBackgroundColor() {
     document.body.style.background = "lightblue"
+}
+
+
+function addEventListeners() {
+    const button =  document.getElementById("cancel")
+    button.addEventListener("click", cancelBackgroundChange)
+}
+
+function cancelBackgroundChange() {
+    clearTimeout(timeoutRef)
 }
