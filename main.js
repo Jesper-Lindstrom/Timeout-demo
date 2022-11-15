2ebad526166cd0e3a1345a17b866ff6ca9ce961b
Jesper Lindström <jesperlindstrom10@gmail.com>
2022-11-15 09:38:25 +0100

    a settimer function for background

---
+++ main.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded" , main);
+
+function main() {
+    // start på programmet
+
+    setTimeout(changeBackgroundColor, 4000);
+}
+
+
+function changeBackgroundColor() {
+    document.body.style.background = "lightblue"
+}
